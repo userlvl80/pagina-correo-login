6bd97fd5391e41af13aea7195decdbe785128177
userlvl80 <106854318+userlvl80@users.noreply.github.com>
2022-08-22 17:43:08 -0500

    Add files via upload

---
+++ correos.php
@@ -0,0 +1,15 @@
+<?php
+    $destinatario = 'joemagrados@gmail.com';
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $email = $_POST['email'];
+
+    $header = "Enviado desde la página de Login al correo"
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+?>
+    
